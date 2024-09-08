db0b0d0f79d4fef6c7d2d4cac525a82ef7f9645c
nope2jope <josephkisbye@gmail.com>
2024-09-08 09:41:34 -0400

    index rework

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-import express from "express";
+var diceQueue = [4, 6, 8, 10, 12, 20];
+var toRoll = 0;
 
-const app = express();
-const port = 3000;
+const diceCount = {
+  4: 0,
+  6: 0,
+  8: 0,
+  10: 0,
+  12: 0,
+  20: 0,
+}
 
+const diceRolls = {
+  4: [],
+  6: [],
+  8: [],
+  10: [],
+  12: [],
+  20: [],
+}
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+const diceSums = {
+  4: 0,
+  6: 0,
+  8: 0,
+  10: 0,
+  12: 0,
+  20: 0,
+}
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+function rollDice(dice, step) {
+  const arr = [];
+  for (var i = 0; i < step; i++) {
+    var roll = Math.floor(Math.random() * dice) + 1;
+    arr.push(roll);
+  }
+  return arr;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  const btnLeftArrow = $("#left-arrow");
+  const btnRightArrow = $("#right-arrow");
+  const btnMinus = $("#minus-button");
+  const btnPlus = $("#plus-button");
+  const btnRoll = $("#roll-button");
+
+  btnRoll.on("click", () => {
+    console.log("click!")
+  });
+
+  btnMinus.on("click", () => {
+    if (toRoll > 0) {
+      toRoll--;
+    }
+  });
+
+  btnPlus.on("click", () => {
+    toRoll++;
+  })
+
+  btnLeftArrow.on("click", () => {
+
+  })
+
+  btnRightArrow.on("click", () => {
+    toRoll++;
+  });
 });
 
-app.listen(port, () => {
-    console.log(`Application is listening on ${port}`)
-  });
